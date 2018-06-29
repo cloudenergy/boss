82eb0eea0bc965c396732614042c22d6ba907046
Jichao Ouyang <oyanglulu@gmail.com>
2018-06-30 00:18:06 +1000

    what's wrong with subscriber

--- dashboard/index.js
+++ dashboard/index.js
@@ -3,11 +3,12 @@ const {drawFundChannelChart} = require('./fundChannel')
 const API = process.env.API_URI || 'http://127.0.0.1:8000'
 const {drawIncomeSummary} = require('./incomeSummary')
 const {drawTopupTrendChart} = require('./topupTrend')
-const {Observable, Subscriber} = require('rxjs/Rx')
+const {Observable} = require('rxjs/Rx')
 
-const subscriber = Subscriber.create(result=>console.debug("updated view:", result),
+
+drawFundChannelChart(API).subscribe(result=>console.debug("updated view:", result),
                                      error=>console.error(error))
-
-drawFundChannelChart(API).subscribe(subscriber)
-drawIncomeSummary(API).subscribe(subscriber)
-drawTopupTrendChart(API).subscribe(subscriber)
+drawIncomeSummary(API).subscribe(result=>console.debug("updated view:", result),
+                                     error=>console.error(error))
+drawTopupTrendChart(API).subscribe(result=>console.debug("updated view:", result),
+                                     error=>console.error(error))
