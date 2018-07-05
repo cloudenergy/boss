0848a138bb4f84c3e23f1868891158c4c8d17091
Jichao Ouyang <oyanglulu@gmail.com>
2018-07-05 18:41:13 +1000

    gen table from data

--- management/src/Project.js
+++ management/src/Project.js
@@ -1,11 +1,36 @@
 import React from 'react'
 import {Observable} from 'rxjs-compat'
 import {API_URI} from './env'
+import * as r from 'ramda'
+
+const tableColumns = [{
+  name: '项目名称',
+  lens: r.lensPath(['project', 'name'])
+},{
+  name: '房间数量',
+  lens: r.lensProp('roomCount')
+},{
+  name: '账号数量',
+  lens: r.lensProp('userCount')
+},{
+  name: '仪表数量',
+  lens: r.lensPath(['device', 'count'])
+},{
+  name: '入住数量',
+  lens: r.lensProp('hehe')
+}]
+
 export default class Project extends React.Component {
   constructor(props) {
     super(props)
     this.state = {
-      projects: []
+      projects: [{
+        project: {name: 'hehe'},
+        roomCount: 100,
+        userCount: 15,
+        device:{count:100},
+        hehe:'hihi'
+      }]
     }
   }
   componentDidMount() {
@@ -14,7 +39,24 @@ export default class Project extends React.Component {
   }
   render() {
     return (
-      <div>Hi</div>
+      <table className="table">
+        <thead>
+          <tr>
+            {tableColumns.map((col, i) =>(
+              <th key={i} scope="col">{col.name}</th>
+            ))}
+          </tr>
+        </thead>
+        <tbody>
+        {this.state.projects.map((project,index)=>(
+          <tr key={index}>
+            {tableColumns.map(col=>(
+              <td>{r.view(col.lens, project)}</td>
+            ))}
+          </tr>
+        ))}
+        </tbody>
+      </table>
     )
   }
 }
