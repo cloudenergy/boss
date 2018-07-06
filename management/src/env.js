fa021cbac7c40cb9869cf1672622a85edf146a89
Jichao Ouyang <oyanglulu@gmail.com>
2018-07-06 22:34:56 +1000

    can toggle api uri from localhost

--- management/src/env.js
+++ management/src/env.js
@@ -1,1 +1,1 @@
-export const API_URI = process.env.API_URI || 'http://127.0.0.1:8000'
+export const API_URI = localStorage.getItem('API_URI') || process.env.REACT_APP_API_URI || 'http://127.0.0.1:8000'
