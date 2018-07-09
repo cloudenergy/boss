3ddeb3417c43f1b162625c5391aa0461b7aa1ca6
Jichao Ouyang <oyanglulu@gmail.com>
2018-07-09 20:12:24 +1000

    show withDraw info

--- management/src/Finance.js
+++ management/src/Finance.js
@@ -3,7 +3,7 @@ import {Observable} from 'rxjs-compat'
 import {API_URI} from './env'
 import * as r from 'ramda'
 
-const tableColumns = [{
+const payChannelTable = [{
   name: '项目名称',
   lens: r.lensPath(['fundChannel', 'project', 'name'])
 },{
@@ -26,43 +26,84 @@ const tableColumns = [{
   lens: r.lensPath(['project', 'description'])
 }]
 
+const withDrawTable =[{
+  name: '项目名称',
+  lens: r.lensPath(['channel', 'project', 'name'])
+},{
+  name: '交易账户',
+  lens: r.lensPath(['channel', 'name'])
+},{
+  name: '提现金额',
+  lens: r.lensProp('amount')
+},{
+  name: '提现服务费',
+  lens: r.lensPath([''])
+},{
+  name: '到账金额',
+  lens: r.lensProp(['meh'])
+},{
+  name: '操作后余额',
+  lens: r.lensPath(['fundChannel', 'createdAt'])
+},{
+  name: '提交时间',
+  lens: r.lensPath(['createdAt'])
+},{
+  name: '操作',
+  lens: r.lensPath(['project', 'description'])
+}]
+
 export default class Finance extends React.Component {
   constructor(props) {
     super(props)
     this.state = {
-      payChannel: [{
-        project: {name: 'hehe'},
-        roomCount: 100,
-        userCount: 15,
-        deviceCount:100,
-        hehe:'hihi'
-      }]
+      payChannel: [],
+      withDraw: []
     }
   }
   componentDidMount() {
     Observable.ajax({url: `${API_URI}/v1.0/boss/finance`,crossDomain:true, withCredentials: true})
-      .subscribe(({response})=>this.setState({payChannel: response}))
+      .subscribe(({response})=>this.setState(response))
   }
    render() {
-    return (
-      <table className="table">
-        <thead>
-          <tr>
-            {tableColumns.map((col, i) =>(
-              <th key={i} scope="col">{col.name}</th>
-            ))}
-          </tr>
-        </thead>
-        <tbody>
-        {this.state.payChannel.map((project,index)=>(
-          <tr key={index}>
-            {tableColumns.map(col=>(
-              <td>{r.view(col.lens, project)}</td>
-            ))}
-          </tr>
-        ))}
-        </tbody>
-      </table>
+     return (
+       <div>
+         <table className="table">
+           <thead>
+             <tr>
+               {payChannelTable.map((col, i) =>(
+                 <th key={i} scope="col">{col.name}</th>
+               ))}
+             </tr>
+           </thead>
+         <tbody>
+         {this.state.payChannel.map((project,index)=>(
+           <tr key={index}>
+             {payChannelTable.map(col=>(
+               <td>{r.view(col.lens, project)}</td>
+             ))}
+           </tr>
+         ))}
+       </tbody>
+         </table>
+         <table className="table">
+         <thead>
+         <tr>
+         {withDrawTable.map((col, i) =>(
+           <th key={i} scope="col">{col.name}</th>
+         ))}
+       </tr>
+         </thead>
+         <tbody>
+         {this.state.withDraw.map((project,index)=>(
+           <tr key={index}>
+             {withDrawTable.map(col=>(
+               <td>{r.view(col.lens, project)}</td>
+             ))}
+           </tr>
+         ))}
+       </tbody>
+         </table>
+         </div>
     )
   }
 }
