a6f8e7bc32b11a764fd7ca0fadb29079c72e5933
Jichao Ouyang <oyanglulu@gmail.com>
2018-07-09 20:57:26 +1000

    compose lens to format view

--- management/src/Finance.js
+++ management/src/Finance.js
@@ -5,51 +5,56 @@ import * as r from 'ramda'
 
 const payChannelTable = [{
   name: '项目名称',
-  lens: r.lensPath(['fundChannel', 'project', 'name'])
+  lens: r.view(r.lensPath(['fundChannel', 'project', 'name']))
 },{
   name: '行卡号/支付宝账号',
-  lens: r.lensPath(['account'])
+  lens: r.view(r.lensPath(['account']))
 },{
   name: '支行名称',
-  lens: r.lensProp('subbranch')
+  lens: r.view(r.lensProp('subbranch'))
 },{
   name: '账户归属区域',
-  lens: r.lensPath(['locate'])
+  lens: r.view(r.lensPath(['locate']))
 },{
   name: '审核',
-  lens: r.lensProp(['fundChannel', 'status'])
+  lens: r.compose(r.ifElse(r.equals('PASSED'), r.always('通过'), r.always('失败')),
+                  r.view(r.lensPath(['fundChannel', 'status'])))
 },{
   name: '申请时间',
-  lens: r.lensPath(['fundChannel', 'createdAt'])
+  lens: r.compose(x=>new Date(x).toLocaleString(),
+                  r.view(r.lensPath(['fundChannel', 'createdAt'])))
 },{
   name: '明细查看',
-  lens: r.lensPath(['project', 'description'])
+  lens: r.always(<button type="button" className="btn btn-link" onClick={e=>console.log(e)}>查看明细</button>)
 }]
 
 const withDrawTable =[{
   name: '项目名称',
-  lens: r.lensPath(['channel', 'project', 'name'])
+  lens: r.view(r.lensPath(['channel', 'project', 'name']))
 },{
   name: '交易账户',
-  lens: r.lensPath(['channel', 'name'])
+  lens: r.view(r.lensPath(['channel', 'name']))
 },{
   name: '提现金额',
-  lens: r.lensProp('amount')
+  lens: r.view(r.lensProp('amount'))
 },{
   name: '提现服务费',
-  lens: r.lensPath([''])
+  lens: r.view(r.lensPath(['']))
 },{
   name: '到账金额',
-  lens: r.lensProp(['meh'])
+  lens: r.view(r.lensProp(['meh']))
 },{
   name: '操作后余额',
-  lens: r.lensPath(['fundChannel', 'createdAt'])
+  lens: r.view(r.lensPath(['fundChannel', 'createdAt']))
 },{
   name: '提交时间',
-  lens: r.lensPath(['createdAt'])
+  lens: r.compose(x=>new Date(x).toLocaleString(),
+                  r.view(r.lensPath(['createdAt'])))
 },{
   name: '操作',
-  lens: r.lensPath(['project', 'description'])
+  lens: r.always(<button type="button" className="btn btn-link" onClick={e=>console.log(e)}>
+                   审核
+                 </button>)
 }]
 
 export default class Finance extends React.Component {
@@ -78,8 +83,8 @@ export default class Finance extends React.Component {
          <tbody>
          {this.state.payChannel.map((project,index)=>(
            <tr key={index}>
-             {payChannelTable.map(col=>(
-               <td>{r.view(col.lens, project)}</td>
+             {payChannelTable.map((col,index)=>(
+               <td key={index}>{col.lens(project)}</td>
              ))}
            </tr>
          ))}
@@ -96,8 +101,8 @@ export default class Finance extends React.Component {
          <tbody>
          {this.state.withDraw.map((project,index)=>(
            <tr key={index}>
-             {withDrawTable.map(col=>(
-               <td>{r.view(col.lens, project)}</td>
+             {withDrawTable.map((col,index)=>(
+               <td key={index}>{col.lens(project)}</td>
              ))}
            </tr>
          ))}
