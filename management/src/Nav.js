29fc0df5b92619de3e6bb6fda229de6d4a908fcb
Jichao Ouyang <oyanglulu@gmail.com>
2018-07-17 21:23:40 +1000

    fix link

--- management/src/Nav.js
+++ management/src/Nav.js
@@ -2,7 +2,7 @@ import React from 'react'
 import { Link } from 'react-router5'
 const Nav = (props) => (
   <nav className="navbar navbar-expand-lg navbar-light bg-light">
-    <a className="navbar-brand" href="/dashboard">BOSS</a>
+    <a className="navbar-brand" href="/">BOSS</a>
     <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
       <span className="navbar-toggler-icon"></span>
     </button>
