9310946744c34a9227bd30cf7e4a2ec6f7d32074
Jichao Ouyang <oyanglulu@gmail.com>
2018-07-26 22:22:37 +1000

    date should be required

--- management/src/audit/Filter.jsx
+++ management/src/audit/Filter.jsx
@@ -7,8 +7,8 @@ const {Context} = AuditContext
 const Filter = (props) => (
   <Context.Consumer>{({table, actions, modalId})=>(
     <div className="col-6">
-      <input className="col-3" type="date" name="from" defaultValue={props.from} onChange={e=>actions.next(AuditAction.Filters({from: e.target.value})) } />
-      <input className="col-3" type="date" name="to" defaultValue={props.to} onChange={e=>actions.next(AuditAction.Filters({to: e.target.value})) }/>
+      <input className="col-3" type="date" name="from" defaultValue={props.from} onChange={e=>actions.next(AuditAction.Filters({from: e.target.value})) } required />
+      <input className="col-3" type="date" name="to" defaultValue={props.to} onChange={e=>actions.next(AuditAction.Filters({to: e.target.value})) } required/>
       <select className="col-3" onChange={e=> actions.next(AuditAction.Filters({status: e.target.value})) }>
         <option value="">全部状态</option>
         {r.toPairs(props.statusMap).map(([key, val], index)=>(
