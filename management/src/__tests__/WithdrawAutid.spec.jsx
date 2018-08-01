2e4cba3005b94bc01e97ba43d78994584868da32
Jichao Ouyang <oyanglulu@gmail.com>
2018-08-01 21:46:24 +1000

    testing reducer

--- management/src/__tests__/WithdrawAutid.spec.jsx
+++ management/src/__tests__/WithdrawAutid.spec.jsx
@@ -5,7 +5,7 @@ import Enzyme, {mount} from 'enzyme'
 import Adapter from 'enzyme-adapter-react-16'
 import {AuditContext,createActionContext} from '../Context'
 import {Observable} from 'rxjs-compat'
-import {rest} from '../utils.js'
+import {rest, now} from '../utils.js'
 jest.mock('../utils')
 import Fuse from 'fuse.js'
 import WithdrawAudit from '../WithdrawAudit';
@@ -13,7 +13,7 @@ import View from '../WithdrawAudit.view.jsx'
 import reducer from '../WithdrawAudit.reducer.jsx'
 const {Context, Var} = AuditContext
 Enzyme.configure({ adapter: new Adapter() })
-
+now.mockReturnValue(new Date('2018-07-28'))
 const data = {withDraw:[
   {
     "id": 2,
@@ -85,7 +85,14 @@ const data = {withDraw:[
       }
     }
   }]}
-rest.mockReturnValue(Observable.of({response:data}))
+rest.mockImplementation(url=>{
+  switch(url){
+    case "environments": return Observable.of({response: [{key:'user', id: 123}]});
+    case 'boss/incomeSummary': return Observable.of({response:{"sum":14409,"withdraw":0,"fee":0,"arrears":[{"value":"-394404","count":11}],"frozen":400,"balance":13922.546}});
+    case 'boss/withDraw': return Observable.of({response:{"withDraw":[{"id":3,"projectId":"6376305154160988160","amount":400,"fundChannelId":26,"operator":"6413312023169470466","auditor":0,"status":"PENDING","createdAt":"2018-07-26T13:24:02.000Z","updatedAt":"2018-07-26T13:24:02.000Z","deletedAt":null,"auth":null,"channel":{"id":26,"flow":"pay","projectId":"6376305154160988160","category":"online","tag":"alipay","name":"丁新城","status":"PASSED","createdAt":"2018-07-18T12:49:52.000Z","updatedAt":"2018-07-20T12:21:36.000Z","deletedAt":null,"project":{"id":"6376305154160988160","logoUrl":null,"name":"测试项目","address":null,"description":null,"telephone":null,"createdAt":"2018-03-05T06:00:23.000Z","updatedAt":"2018-03-05T06:00:27.000Z","deletedAt":null}}},{"id":2,"projectId":"6376305154160988160","amount":1000,"fundChannelId":26,"operator":"6413312023169470466","auditor":"6413312023169470466","status":"PROCESSFAILURE","createdAt":"2018-07-26T13:05:45.000Z","updatedAt":"2018-07-26T13:17:56.000Z","deletedAt":null,"auth":{"id":"6413312023169470466","level":"OP","username":"dxcpuls","password":"c33cc8e44b724bfa0bae09dbc946f8d7","email":"","mobile":null,"allowReceiveFrom":"BOTH","lastLoggedIn":null,"createdAt":"2018-03-05T06:01:03.000Z","updatedAt":"2018-03-05T06:01:05.000Z","deletedAt":null,"projectId":"6376305154160988160"},"channel":{"id":26,"flow":"pay","projectId":"6376305154160988160","category":"online","tag":"alipay","name":"丁新城","status":"PASSED","createdAt":"2018-07-18T12:49:52.000Z","updatedAt":"2018-07-20T12:21:36.000Z","deletedAt":null,"project":{"id":"6376305154160988160","logoUrl":null,"name":"测试项目","address":null,"description":null,"telephone":null,"createdAt":"2018-03-05T06:00:23.000Z","updatedAt":"2018-03-05T06:00:27.000Z","deletedAt":null}}},{"id":1,"projectId":"6376305154160988160","amount":1000,"fundChannelId":26,"operator":"6413312023169470466","auditor":"6413312023169470466","status":"PROCESSFAILURE","createdAt":"2018-07-26T13:03:46.000Z","updatedAt":"2018-07-26T13:17:57.000Z","deletedAt":null,"auth":{"id":"6413312023169470466","level":"OP","username":"dxcpuls","password":"c33cc8e44b724bfa0bae09dbc946f8d7","email":"","mobile":null,"allowReceiveFrom":"BOTH","lastLoggedIn":null,"createdAt":"2018-03-05T06:01:03.000Z","updatedAt":"2018-03-05T06:01:05.000Z","deletedAt":null,"projectId":"6376305154160988160"},"channel":{"id":26,"flow":"pay","projectId":"6376305154160988160","category":"online","tag":"alipay","name":"丁新城","status":"PASSED","createdAt":"2018-07-18T12:49:52.000Z","updatedAt":"2018-07-20T12:21:36.000Z","deletedAt":null,"project":{"id":"6376305154160988160","logoUrl":null,"name":"测试项目","address":null,"description":null,"telephone":null,"createdAt":"2018-03-05T06:00:23.000Z","updatedAt":"2018-03-05T06:00:27.000Z","deletedAt":null}}}]}})
+  }
+
+})
 describe('<WithdrawAudit/>', ()=>{
   let subject, state= {
     withDraw: data.withDraw,
@@ -122,10 +129,10 @@ describe('<WithdrawAudit/>', ()=>{
     expect(renderer.create(subject).toJSON()).toMatchSnapshot()
   })
 
-  it('trigger search', (done) => {
+  it('Action Load', (done) => {
     let setState = jest.fn()
-    reducer(setState, state).subscribe(x=>{
-      console.log(x)
+    reducer(setState, state).take(3).toArray().subscribe(x=>{
+      expect(setState.mock.calls.length).toEqual(3)
       done()
     },err=>console.log(err))
   })
